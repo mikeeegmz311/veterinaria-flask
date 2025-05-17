3b68375e953e58e2f2f8a73d4f46f037bd66d5c1
mikeeegmz311 <ritagomezmiguelangel@gmail.com>
2025-05-17 17:39:08 -0600

    ya me aburri

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi:app --bind 0.0.0.0:8080
+web: gunicorn wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app import app  # cambia "tu_archivo" por el nombre de tu archivo actual, sin .py
+from app import app
 
-# En tu caso actual:
-# from main import app   ← Si el archivo actual se llama main.py
+if __name__ == "__main__":
+    app.run()
